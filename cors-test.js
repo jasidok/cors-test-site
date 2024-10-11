0923ebe0a7edbce05a0555eed90321290d412dd2
Dok <jasimurdock@gmail.com>
2024-10-11 15:53:59 -0600

    remove credentials

--- cors-test.js
+++ cors-test.js
@@ -1,6 +1,5 @@
 fetch('https://us.trip.com/restapi/soa2/14839/json/hotelList', {
     method: 'GET',
-    credentials: 'include',
     headers: {
         'Origin': 'https://jasidok.github.io'
     }
@@ -8,7 +7,9 @@ fetch('https://us.trip.com/restapi/soa2/14839/json/hotelList', {
 .then(response => response.json())
 .then(data => {
     console.log('CORS Test Successful:', data);
+    alert('Data retrieved successfully from us.trip.com: ' + JSON.stringify(data));
 })
 .catch(error => {
     console.error('CORS Test Failed:', error);
+    alert('CORS request failed. Check the console for more details.');
 });
